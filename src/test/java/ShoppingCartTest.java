dcf67e9677ce1bdfb7f5590c9ab0968d03f94e74
Huy Nguyen <huylvl1994@gmail.com>
2019-12-24 00:30:39 +0700

    Finish implementation and unit test

---
+++ src/test/java/ShoppingCartTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import user.User;
+
+public class ShoppingCartTest {
+
+    @Test
+    public void testCreateFirstUser() {
+        User user = new User("John Doe", "john.doe@example.com");
+        user.addToShoppingCart("apple", 2);
+        user.addToShoppingCart("orange", 1);
+        assertEquals(user.getTotalMoney(), 13.89);
+    }
+
+    @Test
+    public void testCreateSecondtUser() {
+        User user = new User("John Doe", "john.doe@example.com");
+        user.addToShoppingCart("apple", 3);
+        user.removeFromShoppingcart("apple", 1);
+        assertEquals(user.getTotalMoney(), 9.9);
+    }
+}
